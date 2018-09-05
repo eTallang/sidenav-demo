7b354044b86a8e61884c170024c2c155d53102d3
Erik Tallang <e.tallang@gmail.com>
2018-09-05 21:11:40 +0200

    feat(sidenav): implement basic sidenav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  iconName: string;
+  name: string;
+  subItems?: MenuItem[];
+}
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent {
+  isOpen = true;
+  menuItems: MenuItem[] = [
+    {
+      iconName: 'home',
+      name: 'Home'
+    },
+    {
+      iconName: 'grid_on',
+      name: 'Sheets'
+    },
+    {
+      iconName: 'assignment',
+      name: 'Assignments',
+      subItems: [
+        {
+          iconName: 'work',
+          name: 'Work assignments'
+        },
+        {
+          iconName: 'email',
+          name: 'Sent assignments'
+        },
+        {
+          iconName: 'add_circle',
+          name: 'Add assignment'
+        }
+      ]
+    },
+    {
+      iconName: 'favorite',
+      name: 'Favorites'
+    },
+    {
+      iconName: 'account_circle',
+      name: 'Profile'
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+  toggleExpansion(): void {
+    this.isOpen = !this.isOpen;
   }
-
 }
