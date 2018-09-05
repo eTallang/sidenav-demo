87264a37049e5317dcf6b5a5f6ebe01a07f7c431
Erik Tallang <e.tallang@gmail.com>
2018-09-05 20:26:58 +0200

    feat(sidenav): add empty sidenav module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidenavModule } from './sidenav/sidenav.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    SidenavModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
